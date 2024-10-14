496c2c6f23d72728382ac45fbbf2dd6cdca0d859
Nik <niklas@kizo-agency.com>
2024-10-14 09:49:40 +0200

    All projects cleaned up into skeleton css svelte-kit projects

---
+++ ebook-landing/src/lib/components/AuthorSection.svelte
@@ -0,0 +1,20 @@
+<style>
+  .author-container {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .author-container img {
+    width: 40%;
+    border: 6px solid black;
+    box-shadow: 0 4px 24px rgba(0, 0, 0, 0.5);
+  }
+
+  .author-text {
+    width: 55%;
+  }
+
+  /* .author-text p:not(:last-of-type) {
+    margin-bottom: 8px;
+  } */
+</style>
